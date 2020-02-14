a944417c04caf0fd402a469d4e4e5be0ff6f35fa
Treinamento <Treinamento@RD723917.raiadrogasil.com>
2020-02-14 14:03:53 -0300

    Realizando Tarefa G - Classe Digital House Manager

---
+++ src/br/com/rd/desafio/control/DigitalHouseManager.java
@@ -0,0 +1,18 @@
+package br.com.rd.desafio.control;
+
+import br.com.rd.desafio.model.Aluno;
+import br.com.rd.desafio.model.Curso;
+import br.com.rd.desafio.model.Matricula;
+import br.com.rd.desafio.model.Professor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DigitalHouseManager {
+    List<Aluno> listaDeAlunos;
+    List<Professor> listaDeProfessores;
+    List<Curso> listaDeCursos;
+    List<Matricula> listaDeMatricula;
+
+
+}
