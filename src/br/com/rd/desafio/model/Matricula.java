e5d6fd5b6c326d120eeb5192755c0ea55e5a64a0
Treinamento <Treinamento@RD723917.raiadrogasil.com>
2020-02-14 12:44:45 -0300

    Criando Classe MAtricula

---
+++ src/br/com/rd/desafio/model/Matricula.java
@@ -0,0 +1,11 @@
+package br.com.rd.desafio.model;
+
+import java.util.Date;
+
+public class Matricula {
+    private Aluno aluno;
+    private Curso curso;
+    private Date dataMatricula;
+
+    
+}
