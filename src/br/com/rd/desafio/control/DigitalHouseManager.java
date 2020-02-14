f3c95736e40373dad59670fcd8edb4ccdb1c4bcf
Treinamento <Treinamento@RD723917.raiadrogasil.com>
2020-02-14 16:41:19 -0300

    Parte I - Classe DigitalHouseManager - AlocarProfessores

--- src/br/com/rd/desafio/control/DigitalHouseManager.java
+++ src/br/com/rd/desafio/control/DigitalHouseManager.java
@@ -3,22 +3,27 @@ package br.com.rd.desafio.control;
 import br.com.rd.desafio.model.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.*;
+import java.util.stream.Collector;
+
 
 public class DigitalHouseManager {
-    List<Aluno> listaDeAlunos;
-    List<Professor> listaDeProfessores;
-    List<Curso> listaDeCursos;
-    List<Matricula> listaDeMatricula;
+    private List<Aluno> listaDeAlunos;
+    private List<Professor> listaDeProfessores;
+    private List<Curso> listaDeCursos;
+    private List<Matricula> listaDeMatricula;
 
-    public DigitalHouseManager(){
+    public DigitalHouseManager() {
         init();
     }
 
-    private void init(){
-       this.listaDeCursos = new ArrayList<>();
-       this.listaDeProfessores = new ArrayList<>();
-       this.listaDeAlunos = new ArrayList<>();
+    private void init() {
+        this.listaDeCursos = new ArrayList<>();
+        this.listaDeProfessores = new ArrayList<>();
+        this.listaDeAlunos = new ArrayList<>();
+        this.listaDeMatricula = new ArrayList<>();
     }
 
 
@@ -27,32 +32,87 @@ public class DigitalHouseManager {
         this.listaDeCursos.add(curso);
     }
 
-    public void excluirCurso(Integer codigoCurso){
+    public void excluirCurso(Integer codigoCurso) {
         this.listaDeCursos.removeIf(curso -> (curso.getCodigo() == codigoCurso));
     }
 
-    public void registrarProfessorAdjunto(String nome, String sobrenome, Integer codigoProfessor, Integer quantidadeDeHoras){
+    public void registrarProfessorAdjunto(String nome, String sobrenome, Integer codigoProfessor, Integer quantidadeDeHoras) {
         ProfessorAdjunto professorAdjunto = new ProfessorAdjunto(nome, sobrenome, codigoProfessor, quantidadeDeHoras);
         this.listaDeProfessores.add(professorAdjunto);
     }
 
-    public void registrarProfessorTitular(String nome, String sobrenome, Integer codigoProfessor, String especialidade){
+    public void registrarProfessorTitular(String nome, String sobrenome, Integer codigoProfessor, String especialidade) {
         ProfessorTitular professorTitular = new ProfessorTitular(nome, sobrenome, codigoProfessor, especialidade);
         this.listaDeProfessores.add(professorTitular);
     }
 
-    public void excluirProfessor(Integer codigoProfessor){
+    public void excluirProfessor(Integer codigoProfessor) {
         this.listaDeProfessores.removeIf(curso -> (curso.getCodigo() == codigoProfessor));
     }
 
-    public void matricularAluno(String nome, String sobrenome, Integer codigoAluno){
+    public void matricularAluno(String nome, String sobrenome, Integer codigoAluno) {
         Aluno aluno = new Aluno(nome, sobrenome, codigoAluno);
         this.listaDeAlunos.add(aluno);
     }
 
+    private Curso localizarCurso(Integer codigCurso) {
+        for (Curso curso : this.listaDeCursos)
+            if (curso.getCodigo() == codigCurso)
+                return curso;
+        return null;
+
+    }
+
+    private Aluno localizarAluno(Integer codigoAluno) {
+        for (Aluno aluno : this.listaDeAlunos)
+            if (aluno.getCodigo() == codigoAluno)
+                return aluno;
+        return null;
+    }
+
+    private Professor localizarProfessor(Integer codigoProfessor) {
+        for (Professor professor : this.listaDeProfessores)
+            if (professor.getCodigo() == codigoProfessor)
+                return professor;
+        return null;
+    }
 
 
+    public void matricularAluno(Integer codigoAluno, Integer codigoCurso) {
 
+//        Localizando Aluno e Curso
+//        Curso cursoProcurado = this.listaDeCursos.stream().filter(curso -> curso.getCodigo() == codigoCurso);
+//        Aluno alunoProcurado = this.listaDeAlunos.stream().filter(aluno -> (aluno.getCodigo() == codigoAluno);
+
+        Curso cursoProcurado = localizarCurso(codigoCurso);
+        Aluno alunoProcurado = localizarAluno(codigoAluno);
+
+
+        //Verificando se o Anulo e o Curso foi encontrado
+        if (alunoProcurado != null && cursoProcurado != null) {
+
+            //Tentando Matricular Aluno no Curso, desde que hajam vagas
+            if (cursoProcurado.adicionarUmAluno(alunoProcurado)) {
+                Matricula matricula = new Matricula(alunoProcurado, cursoProcurado);
+                this.listaDeMatricula.add(matricula);
+                System.out.println("Matrícula realizada com sucesso!");
+            } else {
+                System.out.println("Não foi possível realizar a matrícula pois não há vagas");
+            }
+        }
+    }
+
+    public void alocarProfessores(Integer codigoCurso, Integer codigoProfessorTitular, Integer codigoProfessorAdjunto){
+
+        Professor professorTitularProcurado = localizarProfessor(codigoProfessorTitular);
+        Professor professorAdjuntoProcurado = localizarProfessor(codigoProfessorAdjunto);
+        Curso cursoProcurado = localizarCurso(codigoCurso);
+
+        if(professorTitularProcurado!=null && professorAdjuntoProcurado!=null && cursoProcurado!=null){
+            cursoProcurado.setProfessorAdjunto((ProfessorAdjunto) professorAdjuntoProcurado);
+            cursoProcurado.setProfessorTitular((ProfessorTitular) professorTitularProcurado);
+        }
+    }
 
 
 }
