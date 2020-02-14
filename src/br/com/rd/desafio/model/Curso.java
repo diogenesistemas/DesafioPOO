03eb07adcf6d9ed51e973a5e5c60ae53e8daaaeb
Treinamento <Treinamento@RD723917.raiadrogasil.com>
2020-02-14 11:36:46 -0300

    Realizando Tarefa B - Classe Curso

---
+++ src/br/com/rd/desafio/model/Curso.java
@@ -0,0 +1,44 @@
+package br.com.rd.desafio.model;
+
+import java.util.Objects;
+
+public class Curso {
+    private String nome;
+    private Integer codigo;
+
+    public Curso(){}
+
+    public Curso(String nome, Integer codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Curso curso = (Curso) o;
+        return codigo.equals(curso.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
