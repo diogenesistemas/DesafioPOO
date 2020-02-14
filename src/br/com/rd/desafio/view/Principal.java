e5b176f03b1284996aea96d8435900325cd904c0
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-02-14 16:58:14 -0300

    Parte J - Registrando Professores

--- src/br/com/rd/desafio/view/Principal.java
+++ src/br/com/rd/desafio/view/Principal.java
@@ -1,8 +1,22 @@
 package br.com.rd.desafio.view;
 
 
+import br.com.rd.desafio.control.DigitalHouseManager;
+import br.com.rd.desafio.model.ProfessorAdjunto;
+import br.com.rd.desafio.model.ProfessorTitular;
+
 public class Principal {
     public static void main(String[] args) {
+
+        DigitalHouseManager dh = new DigitalHouseManager();
+
+        dh.registrarProfessorTitular("Fábio", "Araujo", 1, "Programação");
+        dh.registrarProfessorTitular("Claudivan", "Costa", 2, "Programação");
+        dh.registrarProfessorAdjunto("Ricardo", "Marcedo", 3, 40);
+        dh.registrarProfessorAdjunto("Diogo", "Ferreira", 4, 20);
+
+     
+
 
     }
 }
