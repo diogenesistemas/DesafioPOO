06ab87ceaefa7f8a2345a5c4c6675d28c1b79ad3
Treinamento <Treinamento@RD723917.raiadrogasil.com>
2020-02-14 13:55:06 -0300

    Realizando Tarefa F - Classe Matricula

--- src/br/com/rd/desafio/model/Matricula.java
+++ src/br/com/rd/desafio/model/Matricula.java
@@ -1,11 +1,45 @@
 package br.com.rd.desafio.model;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Matricula {
     private Aluno aluno;
     private Curso curso;
     private Date dataMatricula;
 
-    
+    public Matricula() {
+
+    }
+
+    public Matricula(Aluno aluno, Curso curso) {
+        this.aluno = aluno;
+        this.curso = curso;
+        this.dataMatricula = new Date();
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Date getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(Date dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
 }
